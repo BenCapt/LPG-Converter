e82eed4d24346dcb7039f0887fc465b58cf2326d
BenCapt <b.gregorino@libero.it>
2025-04-16 13:22:56 +0200

    Update sw.js

--- sw.js
+++ sw.js
@@ -1,21 +1,23 @@
+const CACHE_NAME = 'lpg-converter-v1';
+const FILES_TO_CACHE = [
+  './',
+  './index.html',
+  './styles.css',
+  './script.js',
+  './manifest.json',
+  './icons/icon-192.png',
+  './icons/icon-512.png'
+];
 
-self.addEventListener('install', function(e) {
-  e.waitUntil(
-    caches.open('lpg-cache').then(function(cache) {
-      return cache.addAll([
-        './',
-        './index.html',
-        './style.css',
-        './script.js'
-      ]);
-    })
+self.addEventListener('install', event => {
+  event.waitUntil(
+    caches.open(CACHE_NAME)
+      .then(cache => cache.addAll(FILES_TO_CACHE))
   );
 });
 
-self.addEventListener('fetch', function(e) {
-  e.respondWith(
-    caches.match(e.request).then(function(response) {
-      return response || fetch(e.request);
-    })
+self.addEventListener('fetch', event => {
+  event.respondWith(
+    caches.match(event.request).then(response => response || fetch(event.request))
   );
 });
